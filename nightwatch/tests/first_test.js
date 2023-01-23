81ab870017ec69a49f722e87ed8d620919a3af5c
christmknd <christmokonda@gmail.com>
2023-01-23 11:47:43 +0100

    First test working

--- nightwatch/tests/first_test.js
+++ nightwatch/tests/first_test.js
@@ -5,18 +5,15 @@ module.exports = {
     .url('https://www.google.com')
     .waitForElementVisible('body', 1000)
     .assert.title('Google')
+    .click('#L2AGLb > div') // selector cookie de google 
     .assert.visible('input[type=text]')
     .setValue('input[type=text]', 'nightwatch')
-    .waitForElementVisible('input[name=btnK]', 1000)
-    },
-    
-    /*
-    'step two: click input' : function (browser) { //step two
-    browser
-    .waitForElementVisible('button[name=btnG]', 1000)
-    .click('input[name=btnG]')
-    .pause(1000)
-    .assert.containsText('#main', 'Night Watch')
-    .end(); //to close the browser session after all the steps
-    }*/
+    .click('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b') // accepter cookie 
+    .assert.visible('#rso > div:nth-child(1) > div.g.Ww4FFb.vt6azd.tF2Cxc > div > div.Z26q7c.UK95Uc.jGGQ5e > div > a > h3') //premier lien 
+    .click('#rso > div:nth-child(1) > div.g.Ww4FFb.vt6azd.tF2Cxc > div > div.Z26q7c.UK95Uc.jGGQ5e > div > a > h3')
+    .assert.visible('body > header > div > div > a > div > span') // le titre nightwatch.js 
+    .assert.urlEquals('https://nightwatchjs.org/')
+
     }
+
+}
